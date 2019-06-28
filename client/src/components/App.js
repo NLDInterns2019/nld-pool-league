f68b21047d9986a8a93513c3f0b932134ade89fc
POLYGON98 <52075132+POLYGON98@users.noreply.github.com>
2019-06-28 13:01:58 +0100

    Split header into headerLeft and headerRight to position navigation correctly

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,23 +4,21 @@ import SubNavBar from "./SubNavBar.js";
 import FixtureTable from "./FixtureTable.js";
 import LeagueTable from "./LeagueTable.js";
 
-class App extends React.Component {
-  render() {
-    return (
-      <div className="app">
-        <Header />
-        <SubNavBar />
-        <div className="content">
-          <div className="contentLeft">
-            <LeagueTable />
-          </div>
-          <div className="contentRight">
-            <FixtureTable />
-          </div>
+const App = () => {
+  return (
+    <div className="app">
+      <Header />
+      <SubNavBar />
+      <div className="content">
+        <div className="contentLeft">
+          <LeagueTable />
+        </div>
+        <div className="contentRight">
+          <FixtureTable />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default App;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,8 +5,12 @@ import Navigator from "./Navigator.js";
 const Header = () => {
   return (
     <div className="header">
-      <h1>POOL MANAGER</h1>
-      <Navigator current="8-Ball" items={["8-Ball", "9-Ball", "Billiards"]} />
+      <div className="headerLeft">
+        <h1>POOL MANAGER</h1>
+      </div>
+      <div className="headerRight">
+        <Navigator current="8-Ball" items={["8-Ball", "9-Ball", "Billiards"]} />
+      </div>
     </div>
   );
 };
