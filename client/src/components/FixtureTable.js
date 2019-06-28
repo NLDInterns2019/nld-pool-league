fadf663fb6d33eacaccc533d3adf25ec394ab23a
POLYGON98 <52075132+POLYGON98@users.noreply.github.com>
2019-06-28 15:20:34 +0100

    Renamed TableBody to LeagueTableBody

--- client/src/components/FixtureTable.js
+++ client/src/components/FixtureTable.js
@@ -1,6 +1,6 @@
 import React from "react";
 import TableHeader from "./TableHeader";
-import TableBody from "./TableBody";
+import TableBody from "./LeagueTableBody";
 
 const FixtureTable = () => {
   var scoreStyle = {
--- client/src/components/LeagueTable.js
+++ client/src/components/LeagueTable.js
@@ -1,16 +1,24 @@
 import React from "react";
 import TableHeader from "./TableHeader";
-import TableBody from "./TableBody";
+import LeagueTableBody from "./LeagueTableBody";
 
-const LeagueTable = (props) => {
+const LeagueTable = props => {
   return (
     <div className="leagueTableContainer">
       <h2>League Table</h2>
       <table className="leagueTable" cellSpacing="0">
-        <TableHeader
-          items={["Pos", "Name", "P", "W", "D", "L", "F", "A", "Pts"]}
-        />
-        <TableBody players={props.players}/>
+        <tr>
+          <th>Pos</th>
+          <th>Name</th>
+          <th>P</th>
+          <th>W</th>
+          <th>D</th>
+          <th>L</th>
+          <th>F</th>
+          <th>A</th>
+          <th>Pts</th>
+        </tr>
+        <LeagueTableBody players={props.players} />
       </table>
     </div>
   );
