e76ec0411f4ecac1f976cc6395e74cba8a41baa9
POLYGON98 <52075132+POLYGON98@users.noreply.github.com>
2019-07-02 13:47:46 +0100

    Changed input in submit score to be a drop-down

--- client/src/components/SubmitScoreForm.js
+++ client/src/components/SubmitScoreForm.js
@@ -8,23 +8,12 @@ class SubmitScoreForm extends Component {
   }
 
   handleSubmit(e) {
-    var regexName = /^[A-Z]+$/; // matches 1 or more uppercase letters
     var regexScore = /^[0-2]$/; // matches 0, 1, or 2
     var score1 = parseInt(document.getElementById("score1").value, 10);
     var score2 = parseInt(document.getElementById("score2").value, 10);
-    var player1 = document.getElementById("player1").value.toUpperCase();
-    var player2 = document.getElementById("player2").value.toUpperCase();
-
-    console.log("Player 1: " + player1);
-    console.log("Player 2: " + player2);
 
     /* check the inputs match the regular expressions */
-    if (
-      !regexName.test(player1) ||
-      !regexName.test(player2) ||
-      !regexScore.test(score1) ||
-      !regexScore.test(score2)
-    ) {
+    if (!regexScore.test(score1) || !regexScore.test(score2)) {
       this.hasInvalidCells = true;
     }
     /* check the two scores entered add up to 2 */
@@ -45,9 +34,15 @@ class SubmitScoreForm extends Component {
       <div className="submitScoreForm">
         <h2>Submit Score</h2>
         <form>
+          <label>Select a fixture:</label>
+          <select>
+            <option value="option1">A vs B</option>
+            <option value="option2">Mal vs Winston</option>
+            <option value="option3">A vs E</option>
+          </select>
+          <br />
           <input type="number" placeholder="Score" id="score1" />
-          <input type="text" placeholder="Player 1" id="player1" />
-          <input type="text" placeholder="Player 2" id="player2" />
+          <label>vs</label>
           <input type="number" placeholder="Score" id="score2" />
           <br />
           <button type="button" onClick={e => this.handleSubmit(e)}>
