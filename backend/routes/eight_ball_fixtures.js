ad7f2f003399fd37db576cad7ca1eda0f8af18f5
Natalie Hargreaves <Natalie_Hargreaves@waters.com>
2019-07-04 15:00:57 +0100

    some progress on get fixture

--- backend/routes/eight_ball_fixtures.js
+++ backend/routes/eight_ball_fixtures.js
@@ -1,8 +1,43 @@
 var express = require('express');
 var router = express.Router();
 
-//GET handler for /api/8ball_fixtures
-router.get('/', (req,res) => {
-    res.send("/api/8ball_fixture")
+const eight_ball_fixtures = require('../models/eight_ball_fixtures');
+
+router.get("/", function (req,res) {
+    // response = knex.select().from('eight_ball_fixtures')
+    // .then(function(fixtures){
+    //     res.json(fixtures)
+    // })
+
+    eight_ball_fixtures.query()
+    .then(fixture => {
+      res.json(fixture)
+    }, e => {
+      res.status(400).json(e);
+    })
 })
+
+/*router.get("/", (req, res) => {
+  eight_ball_leagues.query()
+  .then(players => {
+    res.json(players)
+  }, e => {
+    res.status(400).json(e);
+  })
+});*/
+/*
+//GET 8 BALL FIXTURE
+app.get("/api/8ball_league/fixture", (req, res) => {
+  let where = {};
+
+  db.eight_ball_fixtures.findAll({ where: where }).then(
+    fixtures => {
+      res.json(fixtures);
+    },
+    e => {
+      res.status(400).send();
+    }
+  );
+});
+*/
 module.exports = router;
