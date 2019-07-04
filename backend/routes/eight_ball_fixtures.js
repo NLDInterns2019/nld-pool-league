ca450b120900e920ca6cc4d0f8cd15c85d62eeb3
Natalie Hargreaves <Natalie_Hargreaves@waters.com>
2019-07-04 15:26:28 +0100

    Added POST for fixtures

--- backend/routes/eight_ball_fixtures.js
+++ backend/routes/eight_ball_fixtures.js
@@ -3,12 +3,11 @@ var router = express.Router();
 
 const eight_ball_fixtures = require('../models/eight_ball_fixtures');
 
+/* 
+  POST handler for /api/8ball_fixtures
+  Function: Displays fixtures
+*/
 router.get("/", function (req,res) {
-    // response = knex.select().from('eight_ball_fixtures')
-    // .then(function(fixtures){
-    //     res.json(fixtures)
-    // })
-
     eight_ball_fixtures.query()
     .then(fixture => {
       res.json(fixture)
@@ -16,28 +15,54 @@ router.get("/", function (req,res) {
       res.status(400).json(e);
     })
 })
-
-/*router.get("/", (req, res) => {
-  eight_ball_leagues.query()
-  .then(players => {
-    res.json(players)
-  }, e => {
-    res.status(400).json(e);
-  })
-});*/
 /*
-//GET 8 BALL FIXTURE
-app.get("/api/8ball_league/fixture", (req, res) => {
-  let where = {};
-
-  db.eight_ball_fixtures.findAll({ where: where }).then(
-    fixtures => {
-      res.json(fixtures);
-    },
-    e => {
-      res.status(400).send();
-    }
-  );
+  eight_ball_leagues
+    .query()
+    .insert({ seasonId: body.seasonId, staffName: body.staffName })
+    .then(
+      player => {
+        res.json(player);
+      },
+      e => {
+        res.status(404).json(e);
+      }
+    );
 });
+
+/* 
+  !!!!!!!!!!UNTESTED!!!!!!!!!!
+  POST handler for /api/8ball_fixtures/add/fixture_row
+  Function: Adds a row to a fixture
 */
+app.post("/add/fixture_row", (req, res) => {
+    const body = _.pick(req.body, "seasonId", "score1", "player1", "player2", "score2");
+    const schema = {
+        seasonId: Joi.string().required(),
+        score1: Joi.number(),
+        player1: Joi.string().required(),
+        player2: Joi.string().required(),
+        score2: Joi.number()
+    }
+  
+    //Validation
+    if (Joi.validate(body, schema, { convert: false }).error) {
+        res.status(400).json({ status: 'error', error: 'Invalid data' })
+        return
+    }
+
+    eight_ball_fixtures
+        .query()
+        .patch({
+            score1: score1,
+            score2: score2
+        })
+        .where(
+            'seasonId', ':',  seasonId,
+            'player1', ':', player1,
+            'player2', ':', player2
+        ), e => {
+            res.status(400).json
+        }
+  });
+
 module.exports = router;
