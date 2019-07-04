15b41dac2ecee8c0b860a706097a8d1f8b5caab6
Matthew Pollard <52075132+POLYGON98@users.noreply.github.com>
2019-07-04 11:57:16 +0100

    Removed unnecessary imports from index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App.js";
-import SeasonsPage from "./components/SeasonsPage.js";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import FixturesPage from "./components/FixturesPage.js";
-import LandingPage from "./components/LandingPage.js";
+import { BrowserRouter as Router } from "react-router-dom";
+
 import Routes from "./components/Routes.js";
 
 ReactDOM.render(
