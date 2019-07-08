f72275ec4d53526c613f68157e05d1e16881f329
Matthew Pollard <52075132+POLYGON98@users.noreply.github.com>
2019-07-08 13:13:44 +0100

    Added more tests for Create Season Form

--- client/src/components/__tests__/CreateSeasonForm.test.js
+++ client/src/components/__tests__/CreateSeasonForm.test.js
@@ -5,6 +5,7 @@ import { mount, render, shallow, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import CreateSeasonForm from "../CreateSeasonForm.js";
 import sinon from "sinon";
+import SeasonsPage from "../SeasonsPage.js";
 chai.should();
 
 configure({ adapter: new Adapter() });
@@ -66,12 +67,48 @@ describe("Remove a Player", () => {
 describe("Remove Player button click", () => {
   it("should run removePlayer() function", () => {
     const wrapper = shallow(<CreateSeasonForm />);
-    wrapper.find("#addPlayer").simulate("click");
-    wrapper.find("#addPlayer").simulate("click");
-    const removePlayerBtn = wrapper.find("#button2");
     var spy = sinon.spy(CreateSeasonForm.prototype, "removePlayer");
+
+    /* add 2 players so remove buttons are visible */
+    wrapper.find("#addPlayer").simulate("click");
+    wrapper.find("#addPlayer").simulate("click");
+
+    const removePlayerBtn = wrapper.find("#button2");
 
     removePlayerBtn.simulate("click");
     spy.calledOnce.should.be.true;
+  });
+});
+
+describe("Create Season button click", () => {
+  it("should run createSeason() function", () => {
+    var spy = sinon.spy(CreateSeasonForm.prototype, "createSeason");
+    const wrapper = mount(<CreateSeasonForm />);
+    const createSeasonBtn = wrapper.find("#createSeasonBtn");
+
+    // prevents error with alerts. An alert showing is normal behaviour, not an error
+    window.alert = () => {};
+
+    createSeasonBtn.simulate("click");
+
+    spy.calledOnce.should.be.true;
+  });
+});
+
+describe("Typing a season number", () => {
+  it("should run setSeasonName()", () => {
+    const wrapper = shallow(<CreateSeasonForm />);
+    var spy = sinon.spy(CreateSeasonForm.prototype, "setSeasonName");
+    const event = { target: { value: "1" } };
+    wrapper.find("#inputSeasonNo").simulate("change", event);
+
+    spy.called.should.be.true;
+  });
+
+  it("should change the state", () => {
+    const wrapper = shallow(<CreateSeasonForm />);
+    const event = { target: { value: "1" } };
+    wrapper.find("#inputSeasonNo").simulate("change", event);
+    wrapper.state().seasonName.should.equal("1");
   });
 });
