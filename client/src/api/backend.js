c4679cae389e6d6e452a5a5a7fdc50fb969fbb31
Michael Putra <shura.mike22@yahoo.com>
2019-07-08 14:23:15 +0100

    Backend fix: adding player to the league

--- client/src/api/backend.js
+++ client/src/api/backend.js
@@ -2,5 +2,5 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL: 'http://nldpoolleaguebackend.azurewebsites.net'
-    //baseURL: 'http://localhost:8080'
+    // baseURL: 'http://localhost:8080'
 })
