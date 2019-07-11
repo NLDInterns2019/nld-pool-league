5b745955386285808f32197da258a280df72a56a
Michael Putra <shura.mike22@yahoo.com>
2019-07-11 13:02:00 +0100

    Test seed should not generate any fixtures

--- backend/db/seeds/test/create_fixtures.js
+++ backend/db/seeds/test/create_fixtures.js
@@ -3,19 +3,5 @@ exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex('eight_nine_ball_fixtures').del()
     .then(function () {
-      // Inserts seed entries
-      return knex('eight_nine_ball_fixtures').insert([
-        {type: 8, seasonId: 2019, player1: 'Michael', player2: 'Matthew'},
-        {type: 8, seasonId: 2019, player1: 'Michael', player2: 'Natalie'},
-        {type: 8, seasonId: 2019, player1: 'Matthew', player2: 'Natalie'},
-
-        {type: 8, seasonId: 2020, player1: 'Michael', player2: 'Matthew'},
-        {type: 8, seasonId: 2020, player1: 'Michael', player2: 'Natalie'},
-        {type: 8, seasonId: 2020, player1: 'Matthew', player2: 'Natalie',},
-
-        {type: 9, seasonId: 2019, player1: 'Michael', player2: 'Matthew'},
-        {type: 9, seasonId: 2019, player1: 'Michael', player2: 'Natalie'},
-        {type: 9, seasonId: 2019, player1: 'Matthew', player2: 'Natalie'},
-      ]);
     });
 };
