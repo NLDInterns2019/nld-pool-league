4e4356cd4bf3ddd11b9e15c58c149be1632cb67e
Natalie Hargreaves <Natalie_Hargreaves@waters.com>
2019-07-11 11:26:56 +0100

    numerous bug fixes and tests

--- backend/functions/fixturegen.js
+++ backend/functions/fixturegen.js
@@ -1,27 +1,34 @@
 module.exports = {
-    //calculate fixture values
-    fixtureCalc: function (players, seasonId, group, date) {
-       // let offset = 2;
-        playerCount = players.length;
-        fixture = [];
-        for (var i = 0; i<playerCount/2-1; i++) { //this represents fixture rows
-            fixture = [...fixture, ({
-              seasonId: seasonId,
-              player1: players[i].staffName,
-              player2: players[players.length-i-2].staffName, //2 was offset
-              group: group,
-              date: date
-            })];
-          }
-          if (playerCount%2==0) {
-          fixture = [...fixture, ({
-            seasonId: seasonId,
-            player1: players[playerCount-1].staffName,
-              player2: players[players.length/2-1].staffName,
-              group: group,
-              date: date
-             })]
-          }
-          return fixture;
+  //calculate fixture values
+  fixtureCalc: function(players, seasonId, group, date) {
+    playerCount = players.length;
+    fixture = [];
+    for (var i = 0; i < playerCount / 2 - 1; i++) {
+      //this represents fixture rows
+      fixture = [
+        ...fixture,
+        {
+          seasonId: seasonId,
+          player1: players[i].staffName,
+          player2: players[players.length - i - 2].staffName,
+          group: group,
+          date: date
+        }
+      ];
     }
-}
+    if (playerCount % 2 == 0) {
+      //ʕ•ᴥ•ʔ hi
+      fixture = [
+        ...fixture,
+        {
+          seasonId: seasonId,
+          player1: players[playerCount - 1].staffName,
+          player2: players[players.length / 2 - 1].staffName,
+          group: group,
+          date: date
+        }
+      ];
+    }
+    return fixture;
+  }
+};
