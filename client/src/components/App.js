8fb9b02bb03c50c8180c9d64742d1e9bd411b241
Michael Putra <shura.mike22@yahoo.com>
2019-07-15 10:42:22 +0100

    Move routing to App.js

---
+++ client/src/components/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import LandingPage from "./LandingPage.js";
+import LeaguePage from "./LeaguePage";
+import SeasonsPage from "./SeasonsPage.js";
+import FixturesPage from "./FixturePage";
+import NotFound from "./NotFound";
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route path="*/overview" component={LeaguePage} />
+          <Route path="*/seasons" component={SeasonsPage} />
+          <Route path="*/fixtures" component={FixturesPage} />
+          <Route component={NotFound} />
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,23 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
-import LandingPage from "./components/LandingPage.js";
-import LeaguePage from "./components/LeaguePage";
-import SeasonsPage from "./components/SeasonsPage.js";
-import FixturesPage from "./components/FixturePage";
-import NotFound from "./components/NotFound";
+import App from "./components/App";
 
-ReactDOM.render(
-  <Router>
-    <div>
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route path="*/overview" component={LeaguePage} />
-        <Route path="*/seasons" component={SeasonsPage} />
-        <Route path="*/fixtures" component={FixturesPage} />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
-  </Router>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
