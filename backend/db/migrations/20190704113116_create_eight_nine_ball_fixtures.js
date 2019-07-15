6eb52758b13ec7cea04d4fd1407b7901c0c3eaeb
Natalie Hargreaves <Natalie_Hargreaves@waters.com>
2019-07-15 16:20:08 +0100

    changed date to integer

--- backend/db/migrations/20190704113116_create_eight_nine_ball_fixtures.js
+++ backend/db/migrations/20190704113116_create_eight_nine_ball_fixtures.js
@@ -9,7 +9,7 @@ exports.up = function(knex) {
     table.string("player2").notNullable();
     table.integer("score2");
     table.integer("group");
-    table.string("date");
+    table.integer("date");
     // table.date("dueDate").notNullable();
 
     table
