a31aad309bc9afe5b362bc13aa32824e3bb4ff85
Matthew Pollard <52075132+POLYGON98@users.noreply.github.com>
2019-07-18 11:39:29 +0100

    The round and due date is now displayed in the header of the fixture tables instead of above

--- client/src/components/fixture/FixtureTableHeader.js
+++ client/src/components/fixture/FixtureTableHeader.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-const FixtureTableHeader = () => {
+const FixtureTableHeader = props => {
   return (
     <thead>
       <tr>
-        <th>Score</th>
-        <th>Name</th>
-        <th />
-        <th>Name</th>
-        <th>Score</th>
+        <th colSpan="2" style={{ textAlign: "left" }}>
+          Round {props.round}
+        </th>
+
+        <th colSpan="3" style={{ textAlign: "right" }}>
+          Due:&nbsp;
+          {new Date(parseInt(props.fixtures[0].date))
+            .toString()
+            .split(" ")
+            .slice(1, 4)
+            .join("-")}
+        </th>
       </tr>
     </thead>
   );
