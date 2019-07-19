d389532437c598bd864c1cc4ccd6c1d51b000e03
Matthew Pollard <52075132+POLYGON98@users.noreply.github.com>
2019-07-19 11:26:43 +0100

    Fixed tests for booking page

--- client/src/components/__tests__/BookingPage.test.js
+++ client/src/components/__tests__/BookingPage.test.js
@@ -3,7 +3,7 @@ import chai from "chai";
 import chaiEnzyme from "chai-enzyme";
 import { shallow, configure } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
-import FixturesPage from "../FixturesPage.js";
+import BookingPage from "../BookingPage.js";
 
 chai.should();
 
@@ -12,7 +12,7 @@ chai.use(chaiEnzyme());
 
 describe("FixturePage component", () => {
   it("should render", () => {
-    const wrapper = shallow(<FixturesPage />);
+    const wrapper = shallow(<BookingPage />);
     wrapper.exists().should.be.true;
   });
 });
