8233f4498df19d732e07a46ed4a5ea56d82c090e
Michael Putra <shura.mike22@yahoo.com>
2019-07-19 09:41:56 +0100

    db date is now in datetime variable

--- backend/db/migrations/20190704113116_create_eight_nine_ball_fixtures.js
+++ backend/db/migrations/20190704113116_create_eight_nine_ball_fixtures.js
@@ -9,7 +9,7 @@ exports.up = function(knex) {
     table.string("player2").notNullable();
     table.integer("score2");
     table.integer("group");
-    table.string("date");
+    table.datetime("date");
     table.integer("booked");
 
     table
