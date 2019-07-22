ff703cd5d1f1717a7383ae35921a4a14bb892122
Michael Putra <shura.mike22@yahoo.com>
2019-07-22 10:20:15 +0100

    Fixed test seed file

--- backend/db/seeds/test/01_create_a_season.js
+++ backend/db/seeds/test/01_create_a_season.js
@@ -8,6 +8,7 @@ exports.seed = function(knex) {
         { type: 8, seasonId: 2019},
         { type: 8, seasonId: 2020},
         { type: 9, seasonId: 2019},
+        { type: 9, seasonId: 2020}
       ]);
     });
 };
