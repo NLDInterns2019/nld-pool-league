3449e3df6af200e13eceeaaa1fcf52a523a34e40
Michael Putra <shura.mike22@yahoo.com>
2019-08-01 14:59:57 +0100

    Moving delete reminder to backend

--- backend/routes/slack.js
+++ backend/routes/slack.js
@@ -32,7 +32,7 @@ router.post("/booking", auth.checkJwt, async (req, res) => {
   var date = moment(req.body.start).format("DD-MMM-YYYY");
   var time = moment(req.body.start).format("HH:mm");
 
-  const response = await web.chat.postMessage({
+  await web.chat.postMessage({
     channel: channel,
     attachments: [
       {
@@ -55,13 +55,12 @@ router.post("/booking", auth.checkJwt, async (req, res) => {
           time
       }
     ]
-  });
-
-  if (response) {
-    res.json(response);
-  } else {
-    res.status(400).send;
-  }
+  })
+  .then(response => {
+    res.json(response)
+  }, e=> {
+    res.status(400).send(e);
+  })
 });
 
 /* 
@@ -87,11 +86,11 @@ router.post("/booking/reminder", auth.checkJwt, async (req, res) => {
     .subtract(15, "minutes")
     .unix();
 
-  if (moment() > moment(fifteenMinsBefore)) {
+  if (moment() > moment(req.body.start).subtract(15, "minutes")) {
     res.status(204).send();
     return;
   }
-  const response = await web.chat.scheduleMessage({
+ await web.chat.scheduleMessage({
     channel: channel,
     post_at: fifteenMinsBefore,
     attachments: [
@@ -107,13 +106,42 @@ router.post("/booking/reminder", auth.checkJwt, async (req, res) => {
         text: req.body.player1 + " vs " + req.body.player2 + " at " + time
       }
     ]
-  });
+  })
+  .then(response => {
+    res.json(response)
+  }, e=> {
+    res.status(400).send(e);
+  })
+});
 
-  if (response) {
-    res.json(response);
-  } else {
-    res.status(400).send;
+/* 
+  DELETE handler for /api/slack/booking/reminder
+  Function: To send new booking message
+*/
+router.delete("/booking/reminder", auth.checkJwt, async (req, res) => {
+  const schema = {
+    messageId: Joi.string().required()
+  };
+
+  //Validation
+  if (Joi.validate(req.body, schema, { convert: false }).error) {
+    res.status(400).json({ status: "error", error: "Invalid data" });
+    return;
   }
+
+  await web.chat
+    .deleteScheduledMessage({
+      channel: channel,
+      scheduled_message_id: req.body.messageId
+    })
+    .then(
+      response => {
+        res.json(response);
+      },
+      e => {
+        res.status(400).send(e);
+      }
+    );
 });
 
 module.exports = router;
