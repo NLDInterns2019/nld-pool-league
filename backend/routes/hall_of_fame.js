31455b18fcd2319d8dbb0d55a7884c37ab8e9876
Natalie Hargreaves <Natalie_Hargreaves@waters.com>
2019-08-09 10:25:46 +0100

    changed improvement to only work on last match

--- backend/routes/hall_of_fame.js
+++ backend/routes/hall_of_fame.js
@@ -184,9 +184,9 @@ router.post("/calculate", async (req, res) => {
     });
 
     //calculate wins as suitable regarding improvement HoF
-    if (seasons.length > 3) {
+    if (seasons.length > 2) {
       hofAll[i].winRate = Math.trunc(
-        (hofAll[i].wins * 100) / hofAll[i].plays - 2
+        (hofAll[i].wins * 100) / hofAll[i].plays - 1
       );
     } else {
       hofAll[i].winRate = Math.trunc((hofAll[i].wins * 100) / hofAll[i].plays);
