a88b3d082a752dce3b3594905edfd158c75ca265
Matthew Pollard <52075132+POLYGON98@users.noreply.github.com>
2019-08-12 14:19:51 +0100

    Positions now display 'st', 'nd', 'rd', and 'th' accordingly

--- client/src/components/userPage/CurrentStats.js
+++ client/src/components/userPage/CurrentStats.js
@@ -1,6 +1,24 @@
 import React from "react";
 import Forms from "../league/Forms";
 
+const displayPos = position => {
+  var textToDisplay = "";
+  var regexOne = /^([1-9]*(0|2|3|4|5|6|7|8|9)1)$|^1$/;
+  var regexTwo = /^([1-9]*(0|2|3|4|5|6|7|8|9)2)$|^2$/;
+  var regexThree = /^([1-9]*(0|2|3|4|5|6|7|8|9)3)$|^3$/;
+
+  if (regexOne.test(position)) {
+    textToDisplay = position + "st";
+  } else if (regexTwo.test(position)) {
+    textToDisplay = position + "nd";
+  } else if (regexThree.test(position)) {
+    textToDisplay = position + "rd";
+  } else {
+    textToDisplay = position + "th";
+  }
+  return textToDisplay;
+};
+
 const CurrentStats = props => {
   return (
     <div>
@@ -8,11 +26,15 @@ const CurrentStats = props => {
       <div className="current-stats">
         <div className="current-league-pos">
           <h4>Current League Position:</h4>
-          <div className="stat">{props.getLeaguePos}</div>
+          <div className="stat">
+            <h4>
+              {props.position >= 0 ? displayPos(props.position + 1) : "-"}
+            </h4>
+          </div>
         </div>
         <div className="current-form">
           <h4>Form:</h4>
-          <Forms form={props.form} />
+          <Forms form={props.player ? props.player.form : []} />
         </div>
       </div>
     </div>
