5541e11fdf336677258a0741b4442f99b6fb3cc9
Matthew Pollard <52075132+POLYGON98@users.noreply.github.com>
2019-08-12 13:30:39 +0100

    Added headings to dashboard

--- client/src/components/userPage/AllTimeStats.js
+++ client/src/components/userPage/AllTimeStats.js
@@ -2,14 +2,17 @@ import React from "react";
 
 const AllTimeStats = props => {
   return (
-    <div className="all-time-stats">
-      <div className="points-per-game">
-        <h4>Avg. Points Per Game:</h4>
-        <div className="stat">{props.getPPG}</div>
-      </div>
-      <div className="win-percentage">
-        <h4>Win Percentage:</h4>
-        <div className="stat">{props.getWinPercentage}</div>
+    <div>
+      <h2 style={{ textAlign: "left", marginLeft: "18px" }}>All-time:</h2>
+      <div className="all-time-stats">
+        <div className="points-per-game">
+          <h4>Avg. Points Per Game:</h4>
+          <div className="stat">{props.getPPG}</div>
+        </div>
+        <div className="win-percentage">
+          <h4>Win Percentage:</h4>
+          <div className="stat">{props.getWinPercentage}</div>
+        </div>
       </div>
     </div>
   );
--- client/src/components/userPage/CurrentStats.js
+++ client/src/components/userPage/CurrentStats.js
@@ -2,16 +2,19 @@ import React from "react";
 import Forms from "../league/Forms";
 
 const CurrentStats = props => {
-  console.log(props.form)
+  console.log(props.form);
   return (
-    <div className="current-stats">
-      <div className="current-league-pos">
-        <h4>Current League Position:</h4>
-        <div className="stat">{props.getLeaguePos}</div>
-      </div>
-      <div className="current-form">
-        <h4>Form:</h4>
-        <Forms form={props.form} />
+    <div>
+      <h2 style={{ textAlign: "left", marginLeft: "18px" }}>Current Season:</h2>
+      <div className="current-stats">
+        <div className="current-league-pos">
+          <h4>Current League Position:</h4>
+          <div className="stat">{props.getLeaguePos}</div>
+        </div>
+        <div className="current-form">
+          <h4>Form:</h4>
+          <Forms form={props.form} />
+        </div>
       </div>
     </div>
   );
