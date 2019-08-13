f1d99558226043865940675beff9aa84bfcf31f4
Matthew Pollard <52075132+POLYGON98@users.noreply.github.com>
2019-08-13 13:57:12 +0100

    Correct player names now appear in drop down when adding player

--- client/src/components/league/AddPlayerForm.js
+++ client/src/components/league/AddPlayerForm.js
@@ -1,14 +1,51 @@
 import React, { Component } from "react";
+import { orderBy, uniq, filter } from "lodash";
+import auth0Client from "../../Auth";
+import backend from "../../api/backend";
 
 class AddPlayerForm extends Component {
   constructor(props) {
     super(props);
+
+    this.initialState = {
+      auth0Players: [],
+      playersName: [],
+      isAuthenticated: false
+    };
+
+    this.state = this.initialState;
 
     this.addPlayerBtn = React.createRef();
     this.hiddenForm = React.createRef();
     this.confirmBtn = React.createRef();
     this.inputPlayer = React.createRef();
   }
+
+  getPlayers = async () => {
+    try {
+      await backend
+        .get("/api/89ball_season/playersdb", {
+          params: {},
+          headers: { Authorization: `Bearer ${auth0Client.getIdToken()}` }
+        })
+        .then(res => {
+          this.setState({
+            auth0Players: orderBy(res.data, ["username"], ["asc"]),
+            playersName: orderBy(this.props.players, ["staffName"], ["asc"])
+              .filter(player => player.staffName !== "admin")
+              .map(player => player.staffName),
+            isAuthenticated: true
+          });
+        });
+    } catch (e) {
+      if (e.response.status === 401) {
+        this.toastUnauthorised();
+      }
+      if (e.response.status === 400) {
+        this.toastError("Something went wrong. Please try again");
+      }
+    }
+  };
 
   handleClick = event => {
     if (this.hiddenForm.current.style.display === "none") {
@@ -30,9 +67,15 @@ class AddPlayerForm extends Component {
     }
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.isAuthenticated === false && auth0Client.isAuthenticated()) {
+      this.getPlayers();
+    }
+  }
+
   render() {
     return (
-      <div class="addPlayerForm">
+      <div className="addPlayerForm">
         <button
           type="button"
           onClick={this.handleClick}
@@ -46,12 +89,22 @@ class AddPlayerForm extends Component {
           style={{ display: "none" }}
         >
           <h3>Add New Player</h3>
-          <label>Select player:</label>
+          <label>Select a player:</label>
           <select ref={this.inputPlayer}>
-            <option>Player 1</option>
-            <option>Player 2</option>
-            <option>Player 3</option>
+            {filter(
+              this.state.auth0Players,
+              p =>
+                !this.state.playersName.includes(p.username) &&
+                p.username !== "admin"
+            ).map(player => {
+              return (
+                <option key={player.username} value={player.username}>
+                  {player.username}
+                </option>
+              );
+            })}
           </select>
+
           <div className="addPlayerFormButtons">
             <button type="button" onClick={this.handleClick}>
               Cancel
