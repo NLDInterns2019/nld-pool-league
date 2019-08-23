67c1f191345eb36dc1ceebbba805817c1829d1c1
nhargreaves <n.hargreaves95@gmail.com>
2019-08-23 14:48:22 +0100

    changed to calculate quant increase

--- backend/routes/hall_of_fame.js
+++ backend/routes/hall_of_fame.js
@@ -149,12 +149,14 @@ router.post("/updateclosed", async (req, res) => {
       console.log(hofAll[j].improvement + " I")
       console.log(oldWinRate + " O")
       console.log(currentWinRate + " C")
-
-      hofAll[j].improvement = oldWinRate - currentWinRate;
+      ((current- old) / old) * 100
+      //hofAll[j].improvement = ((currentWinRate - oldWinRate) / oldWinRate) * 100
+      hofAll[j].improvement = currentWinRate - oldWinRate;
+      //hofAll[j].improvement = oldWinRate - currentWinRate;
       console.log(hofAll[j].improvement + " = " + oldWinRate + " - " + currentWinRate)
       console.log(hofAll[j].improvement + " / " + oldWinRate + " * 100 = ")
 
-      hofAll[j].improvement = (hofAll[j].improvement/oldWinRate) * 100
+      //hofAll[j].improvement = (hofAll[j].improvement/oldWinRate) * 100
       console.log(hofAll[j].improvement)
     } else {
       hofAll[j].improvement = 0; 
