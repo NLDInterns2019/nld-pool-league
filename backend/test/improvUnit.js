e30c84e7070ee63298c2891572e1114a4ab514e5
nhargreaves <n.hargreaves95@gmail.com>
2019-08-28 14:25:00 +0100

    further testing/refactor

---
+++ backend/test/improvUnit.js
@@ -0,0 +1,11 @@
+const expect = require("chai").expect;
+process.env.NODE_ENV = "test";
+const improv = require("../functions/improvement");
+
+describe("improvement()", () => {
+    beforeEach(() => {
+        var players = ['A', 'B', 'C', 'D']
+    })
+    it("should properly calculate an improvement rate", () => {
+    })
+});
