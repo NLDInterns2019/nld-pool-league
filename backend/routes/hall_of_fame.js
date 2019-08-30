8f939117ce92c5560969c1bce008a984bdd3e6d3
Natalie Hargreaves <Natalie_Hargreaves@waters.com>
2019-08-30 11:13:51 +0100

    restored improvement

--- backend/routes/hall_of_fame.js
+++ backend/routes/hall_of_fame.js
@@ -368,9 +368,41 @@ router.post("/updateclosed", async (req, res) => {
     //GUARD
     if (loc >= 0) {
       
-      hofAll = improvement.improvementCalc(currentLeague, pastLeague, hofAll);
+      /////////////////////////////////////
+      let currentWinRate, oldWinRate, totalWins, totalPlays, totalPoints = 0;
+      let present = false;
+
+      //calculate winrate for the current league
+      currentWinRate = (currentLeague[loc].win * 100) / currentLeague[loc].play;
+
+    //count relevant data for past leagues
+    for (let i = 0; i < pastLeagues.length; i++) {
+      if (pastLeagues[i].staffName === hofAll[j].staffName) {
+        present = true;
+        totalWins = totalWins + pastLeagues[i].win;
+        totalPlays = totalPlays + pastLeagues[i].play;
+        totalPoints = totalPoints + pastLeagues[i].points;
+      }
+    }
+
+    //if user has past league matches, calculate their improvement. if not, set it to 0.
+    if (present === true) {
+      //calculate the winrate of the past leagues hofAll.improvement = oldWinRate
+      oldWinRate = (totalWins * 100) / totalPlays;
+
+      //get % increase/decrease
+      hofAll[j].improvement = currentWinRate - oldWinRate;
+    } else {
+      hofAll[j].improvement = 0;
+    }
+
+    //get avg points per season
+    hofAll[j].avgPointsSeason = totalPoints / seasons.length;
+    ////////////////////
+      //hofAll = improvement.improvementCalc(currentLeague, pastLeague, hofAll, loc);
       
       //these have to be deleted so that they don't overwrite the data
+      //WHAT is this why does hofAll need you to delete the fields for league? what have i done
       delete hofAll[j].seasonId;
       delete hofAll[j].id;
       delete hofAll[j].play;
